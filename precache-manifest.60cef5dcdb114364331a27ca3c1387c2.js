1fb9c24384101b7f6f63d1477084c66f98897997
Priyank1712 <pharjilawala07@gmail.com>
2023-06-25 22:37:44 -0700

    Updates

---
+++ precache-manifest.60cef5dcdb114364331a27ca3c1387c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deda2823fef3c6d04c5010a9b003cd5d",
+    "url": "/priyank-harjilawala/index.html"
+  },
+  {
+    "revision": "29084ebee661d5818f3d",
+    "url": "/priyank-harjilawala/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "0f52c7b408441897f889",
+    "url": "/priyank-harjilawala/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "29084ebee661d5818f3d",
+    "url": "/priyank-harjilawala/static/js/2.8472e861.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/priyank-harjilawala/static/js/2.8472e861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f52c7b408441897f889",
+    "url": "/priyank-harjilawala/static/js/main.795aaba5.chunk.js"
+  },
+  {
+    "revision": "dd6bb8fc0f312ed46bc1",
+    "url": "/priyank-harjilawala/static/js/runtime-main.60525c8c.js"
+  }
+]);
